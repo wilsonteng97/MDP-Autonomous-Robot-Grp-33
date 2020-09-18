9da0af0c26e7938a59cac75a0484592e64124da5
xiaoqing872 <61306326+xiaoqing872@users.noreply.github.com>
2020-09-18 16:09:21 +0800

    image rec update

--- RPI/ImageProcessingServer/app/client.py
+++ RPI/ImageProcessingServer/app/client.py
@@ -1,14 +1,20 @@
 import cv2
 import imagezmq
-image_processing_server_url = 'tcp://169.254.243.236:5555'
+import socket
+
+image_processing_server_url = 'tcp://192.168.33.96:5555'
 image_sender = imagezmq.ImageSender(connect_to=image_processing_server_url)
 
-img = cv2.imread('raw-images/id_14 (Y).jpg')
+print("Loading image...")
+img = cv2.imread(r'C:\\\\Users\\xiaoqing\\Documents\\school stuff\\Year 3 Sem 1\\CZ3004 MDP\\image-rec-2\\images\\id_13.jpg')
+print("Image loaded!")
+rpi_name = socket.gethostname()
+print(rpi_name)
 reply = image_sender.send_image(
-                        'image from RPi',
+                        rpi_name,
                         img
                     )
-
+print("Image sent!")
 reply = reply.decode('utf-8')
 
 print(reply)
