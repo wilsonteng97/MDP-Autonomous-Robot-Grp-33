ca6a057c0a4918e35d11e716a5a8791c801909a8
Exi0n97 <48261015+Exi0n97@users.noreply.github.com>
2020-09-28 11:29:43 +0800

    TESTING

--- Arduino/arduino_checklist/sensorlib/StraightChecklist.ino
+++ Arduino/arduino_checklist/sensorlib/StraightChecklist.ino
@@ -1,171 +1,57 @@
-#include "DualVNH5019MotorShield.h"
-#include "PinChangeInterrupt.h"
+#include <MsTimer2.h>
+#include <PID_v1.h>
+#include <PID_AutoTune_v0.h>
+#include <EnableInterrupt.h>
+#include <DualVNH5019MotorShield.h>
 
-#define LEFT_ENCODER  3
-#define RIGHT_ENCODER 11
-
-// Motor timing
-unsigned long currTime = 0;       // updated on every loop
-unsigned long startTimeL = 0;    // start timing A interrupts
-unsigned long startTimeR = 0;    // start timing B interrupts
-unsigned long tickCountL = 0;     // count the A interrupts
-unsigned long tickCountR = 0;     // count the B interrupts
-unsigned long distanceTicksL = 0;          //distance L
-unsigned long distanceTicksR = 0;          //distance R
-unsigned long timeWidthL = 0;              // motor A period
-unsigned long timeWidthR = 0;              // motor B period                  
-unsigned long printTime = 0;
+const int LEFT_PULSE = 3; // LEFT M1 Pulse
+const int RIGHT_PULSE = 11; // RIGHT M2 Pulse
+const int MOVE_FAST_SPEED = 375;
 const int MOVE_MAX_SPEED = 310;
 const int MOVE_MIN_SPEED = 200;
+const int TURN_MAX_SPEED = 260;
+const int ROTATE_MAX_SPEED = 150;
 const int TURN_TICKS_L = 770;
 const int TURN_TICKS_R = 763;
+const int TICKS[10] = {545, 1155, 1760, 2380, 2985, 3615, 4195, 4775, 5370};
+const double DIST_WALL_CENTER_BOX = 1.58;
+const double kp = 0.5, ki = 0, kd = 0;
 
-float setDistance = 10;
-float setTickDistance = 0;
-float setRotation = 45;
+int TENCM_TICKS_OFFSET = 0;
 
-const unsigned long SAMPLE_COUNT = 20;
-
-// PID   
-const unsigned long SAMPLE_TIME = 1000;  // time between PID updates  
-const unsigned long INT_COUNT = 20;     // sufficient interrupts for accurate timing  
-double setpointInit;
-double setpointL = 150;         // setpoint is rotational speed in Hz  
-double inputL = 0;              // input is PWM to motors 
-double prevOutputL = 0; 
-double outputL = 0;             // output is rotational speed in Hz  
-double setpointR = 150;         // setpoint is rotational speed in Hz  
-double inputR = 0;              // input is PWM to motors  
-double prevOutputR = 0;
-double outputR = 0;             // output is rotational speed in Hz  
-double outputSpeedL = 0;
-double outputSpeedR = 0;
-double KpL = 2.5, KiL = 0.1, KdL = 0.1;  
-double KpR = 4, KiR = 0.1, KdR = 0.07; 
-double kL1 = KpL + KiL + KdL;
-double kL2 = -KpL - 2 * KdL;
-double kL3 = KdL; 
-double kR1 = KpR + KiR + KdR;
-double kR2 = -KpR - 2 * KdR;
-double kR3 = KdR; 
-double errorL1 = 0;
-double errorL2 = 0;
-double errorL3 = 0;
-double errorR1 = 0;
-double errorR2 = 0;
-double errorR3 = 0;
-double currRPML;
-double currRPMR;
-
-/*double Kp = 0, Ki = 0, Kd = 0;
-double k1 = Kp + Ki + Kd;
-double k2 = -Kp - 2 * Kd;
-double k3 = Kd;*/
+double tick_R = 0;
+double tick_L = 0;
+double speed_O = 0;
+double previous_tick_R = 0;
+double previous_error = 0;
 
 DualVNH5019MotorShield md;
+PID myPID(&tick_R, &speed_O, &tick_L, kp, ki, kd, DIRECT);
 
-
-void setup() {
-
-  Serial.begin(9600);
+//--------------------------Motor Codes-------------------------------
+void setupMotorEncoder() {
   md.init();
-  pinMode (LEFT_ENCODER, INPUT); //set digital pin 3 as input
-  pinMode (RIGHT_ENCODER, INPUT); //set digital pin 11 as input
-  attachPCINT(digitalPinToPCINT(LEFT_ENCODER), incTicksL, HIGH);
-  attachPCINT(digitalPinToPCINT(RIGHT_ENCODER), incTicksR, HIGH);
-  pinMode(A3, INPUT);
-  pinMode(A0, INPUT);
-  pinMode(A1, INPUT);
-  pinMode(A2, INPUT);
-  pinMode(A4, INPUT);
-  pinMode(A5, INPUT);
-  delay(3000);
-  setpointInit = 80;
-  setTickDistance = distToTicks(setDistance);
-  md.setSpeeds(-calculateSpeedL(setpointInit), -calculateSpeedR(setpointInit));
-  prevOutputL = calculateSpeedL(setpointInit);
-  prevOutputR = calculateSpeedR(setpointInit);
-  stopIfFault();
-  startTimeL = micros();
-  startTimeR = micros();
-  printTime = micros();
-
+  pinMode(LEFT_PULSE, INPUT);
+  pinMode(RIGHT_PULSE, INPUT);
+  enableInterrupt(LEFT_PULSE, leftMotorTime, CHANGE);
+  enableInterrupt(RIGHT_PULSE, rightMotorTime, CHANGE);
 }
 
-void loop() {
-  
-  currTime = micros();
-  readSensors();
-  if (getFrontIR1_block()==2 && getFrontIR3_block()==2)
-  { 
-    md.setBrakes(400, 400);
-    md.setSpeeds(0, 0);
-    delay(1000);
-    
-  }  
- if ((currTime - printTime) >=100000) {
-    currRPML = calculateRPM(timeWidthL);
-    currRPMR = calculateRPM(timeWidthR); 
-    pidController();
-    printTime = currTime;
-    stopIfFault();
-  }
-  
-  
+void stopMotorEncoder() {
+  disableInterrupt(LEFT_PULSE);
+  disableInterrupt(RIGHT_PULSE);
 }
 
-void incTicksL() {
-  tickCountL++;
-  if (tickCountL >= SAMPLE_COUNT) {
-    timeWidthL = currTime - startTimeL;
-    startTimeL = currTime;
-    tickCountL = 0;
-  }
-  distanceTicksL++;
-}
-
-void incTicksR() {
-  tickCountR++;
-  if (tickCountR >= SAMPLE_COUNT) {
-    timeWidthR = currTime - startTimeR;
-    startTimeR = currTime;
-    tickCountR = 0;
-  }
-  distanceTicksR++;
-}
-double calculateRPM(double timeWidth) {
-  double rpm = 60000000 / ((double)timeWidth / (double)SAMPLE_COUNT) / 562.25 / 2;
-  return rpm;
-}
-
-double calculateTimeWidth(double rpm) {
-  double timeWidth = rpm * 2 * 562.25 / (double)SAMPLE_COUNT / 60000000;
-}
-
-double calcRPMfromSpeedL(double speedL) {
-  double rpmL = 0.3019 * speedL - 7.3771;
-  return rpmL;
-}
-
-double calcRPMfromSpeedR(double speedR) {
-  double rpmR = 0.2876 * speedR - 7.1982;
-  return rpmR;
-}
-
-double calculateSpeedL(double rpm) {
-  double speedL = 3.3079 * rpm + 24.723 - 5;
-  return speedL;
-}
-
-double calculateSpeedR(double rpm) {
-  double speedR = 3.4714 * rpm + 25.354;
-  return speedR;
+void setupPID() {
+  myPID.SetMode(AUTOMATIC);
+  myPID.SetOutputLimits(-370, 370);
+  myPID.SetSampleTime(5);
 }
 
 void moveForward(int distance) {
   initializeTick();
   initializeMotor_Start();
-  distance = distToTicks(distance);
+  distance = cmToTicks(distance);
   double currentSpeed = 0;
   if (distance < 60) {
     currentSpeed = MOVE_MIN_SPEED;
@@ -189,49 +75,357 @@ void moveForward(int distance) {
   initializeMotor_End();
 }
 
-void pidController() {
-  
-  Serial.println("PID Controller is running...");
-  Serial.print("Setpoint RPM = ");
-  Serial.println(setpointInit);
+void moveBackwards(int distance) {
+  initializeTick();
+  initializeMotor_Start();
+  distance = cmToTicks(distance);
+  double currentSpeed = 0;
+  if (distance < 60) {
+    currentSpeed = MOVE_MIN_SPEED;
+  } else {
+    currentSpeed = MOVE_MAX_SPEED;
+  }
+  double offset = 0;
+  long last_tick_R = 0;
+  while (tick_R <= distance || tick_L <= distance) {
+    if ((tick_R - last_tick_R) >= 10 || tick_R == 0 || tick_R == last_tick_R) {
+      last_tick_R = tick_R;
+      offset += 0.1;
+    }
+    if (myPID.Compute() || tick_R == last_tick_R) {
+      if (offset >= 1)
+        md.setSpeeds(-(currentSpeed + speed_O), -(currentSpeed - speed_O));
+      else
+        md.setSpeeds(-(offset * (currentSpeed + speed_O)), -(offset * (currentSpeed - speed_O)));
+    }
+  }
+  initializeMotor_End();
+}
 
-  errorL1 = setpointInit - currRPML;
-  errorR1 = setpointInit - currRPMR;
-  
-  //PID control law
-  
-  
-  outputL = prevOutputL + kL1 * errorL1 + kL2 * errorL2 + kL3 * errorL3;
-  outputR = prevOutputR + kR1 * errorR1 + kR2 * errorR2 + kR3 * errorR3;
+void moveForwardFast(int distance) {
+  initializeTick();
+  initializeMotor_Start();
+  distance = cmToTicks(distance);
+  double currentSpeed = 0;
+  if (distance < 30) {
+    currentSpeed = MOVE_MAX_SPEED;
+  } else {
+    currentSpeed = MOVE_FAST_SPEED;
+  }
+  double offset = 0;
+  int last_tick_R = 0;
+  while (tick_R <= distance || tick_L <= distance) {
+    if (distance - tick_R < 150)
+      currentSpeed = 150;
+    if ((tick_R - last_tick_R) >= 10 || tick_R == 0 || tick_R == last_tick_R) {
+      last_tick_R = tick_R;
+      offset += 0.1;
+    }
+    if (myPID.Compute() || tick_R == last_tick_R) {
+      if (offset >= 1)
+        md.setSpeeds(currentSpeed + speed_O, currentSpeed - speed_O);
+      else
+        md.setSpeeds(offset * (currentSpeed + speed_O), offset * (currentSpeed - speed_O));
+    }
+  }
+  initializeMotor_End();
+}
 
-  outputSpeedL = calculateSpeedL(outputL);
-  outputSpeedR = calculateSpeedR(outputR);
+void moveBackwardsFast(int distance) {
+  initializeTick();
+  initializeMotor_Start();
+  distance = cmToTicks(distance);
+  double currentSpeed = 0;
+  if (distance < 30) {
+    currentSpeed = MOVE_MAX_SPEED;
+  } else {
+    currentSpeed = MOVE_FAST_SPEED;
+  }
+  double offset = 0;
+  long last_tick_R = 0;
+  while (tick_R <= distance || tick_L <= distance) {
+    if (distance - tick_R < 150)
+      currentSpeed = 150;
+    if ((tick_R - last_tick_R) >= 10 || tick_R == 0 || tick_R == last_tick_R) {
+      last_tick_R = tick_R;
+      offset += 0.1;
+    }
+    if (myPID.Compute() || tick_R == last_tick_R) {
+      if (offset >= 1)
+        md.setSpeeds(-(currentSpeed + speed_O), -(currentSpeed - speed_O));
+      else
+        md.setSpeeds(-(offset * (currentSpeed + speed_O)), -(offset * (currentSpeed - speed_O)));
+    }
+  }
+  initializeMotor_End();
+}
 
-  md.setSpeeds(-outputL, -outputR);
+void moveForwardCalibrate(int distance) {
+  initializeTick();
+  initializeMotor_Start();
+  distance = cmToTicksCalibrate(distance);
+  if (distance < 3)
+    return;
+  double currentSpeed = 0;
+  //  Serial.print("HELLO: ");
+  //  Serial.println(distance);
+  if (distance < 60) {
+    currentSpeed = MOVE_MIN_SPEED;
+  } else {
+    currentSpeed = MOVE_MAX_SPEED;
+  }
+  double offset = 0;
+  int last_tick_R = 0;
+  while (tick_R <= distance || tick_L <= distance) {
+    if ((tick_R - last_tick_R) >= 10 || tick_R == 0 || tick_R == last_tick_R) {
+      last_tick_R = tick_R;
+      offset += 0.1;
+    }
+    if (myPID.Compute() || tick_R == last_tick_R) {
+      if (offset >= 1 || distance < 110)
+        md.setSpeeds(currentSpeed + speed_O, currentSpeed - speed_O);
+      else
+        md.setSpeeds(offset * (currentSpeed + speed_O), offset * (currentSpeed - speed_O));
+    }
+  }
+  initializeMotor_End();
+}
 
-  prevOutputL = outputL;
-  prevOutputR = outputR;
-  
-  errorL3 = errorL2;
-  errorL2 = errorL1;
-  errorR3 = errorR2;
-  errorR2 = errorR1;
-  
+
+void moveBackwardsCalibrate(int distance) {
+  initializeTick();
+  initializeMotor_Start();
+  distance = cmToTicksCalibrate(distance);
+  if (distance < 3)
+    return;
+  double currentSpeed = 0;
+  //  Serial.print("HELLO: ");
+  //  Serial.println(distance);
+  if (distance < 60) {
+    currentSpeed = MOVE_MIN_SPEED;
+  } else {
+    currentSpeed = MOVE_MAX_SPEED;
+  }
+  double offset = 0;
+  long last_tick_R = 0;
+  while (tick_R <= distance || tick_L <= distance) {
+    if ((tick_R - last_tick_R) >= 10 || tick_R == 0 || tick_R == last_tick_R) {
+      last_tick_R = tick_R;
+      offset += 0.1;
+    }
+    if (myPID.Compute() || tick_R == last_tick_R) {
+      if (offset >= 1 || distance < 110)
+        md.setSpeeds(-(currentSpeed + speed_O), -(currentSpeed - speed_O));
+      else
+        md.setSpeeds(-(offset * (currentSpeed + speed_O)), -(offset * (currentSpeed - speed_O)));
+    }
+  }
+  initializeMotor_End();
+}
+
+void turnLeft() {
+  initializeTick();
+  initializeMotor_Start();
+  double currentSpeed = TURN_MAX_SPEED;
+  double offset = 0;
+
+  while (tick_R < TURN_TICKS_L || tick_L < TURN_TICKS_L) {
+    //    offset = computePID();
+    if (myPID.Compute())
+      md.setSpeeds(-(currentSpeed + speed_O), currentSpeed - speed_O);
+  }
+  initializeMotor_End();
+  initializeLeftTurnEnd();
+}
+
+void turnRight() {
+  initializeTick();
+  initializeMotor_Start();
+  double currentSpeed = TURN_MAX_SPEED;
+  double offset = 0;
+
+  while (tick_R < (TURN_TICKS_R + 10) || tick_L < (TURN_TICKS_R + 10)) {
+    //    offset = computePID();
+    if (myPID.Compute())
+      md.setSpeeds((currentSpeed + speed_O), -(currentSpeed - speed_O));
+  }
+  initializeMotor_End();
+  initializeRightTurnEnd();
+}
+
+void rotateLeft(int distance) {
+  initializeTick();
+  initializeMotor_Start();
+  double currentSpeed = ROTATE_MAX_SPEED;
+  double offset = 0;
+  if (distance < 3)
+    return;
+  while (tick_R < distance || tick_L < distance) {
+    //    offset = computePID();
+    if (myPID.Compute())
+      md.setSpeeds(-(currentSpeed + speed_O), currentSpeed - speed_O);
+  }
+  initializeMotor_End();
+}
+
+void rotateRight(int distance) {
+  initializeTick();
+  initializeMotor_Start();
+  double currentSpeed = ROTATE_MAX_SPEED;
+  double offset = 0;
+  if (distance < 3)
+    return;
+  while (tick_R < distance || tick_L < distance) {
+    //    offset = computePID();
+    if (myPID.Compute())
+      md.setSpeeds((currentSpeed + speed_O), -(currentSpeed - speed_O));
+  }
+  initializeMotor_End();
+}
+
+void alignRight() {
+  delay(2);
+  double diff = getRightIR1() - getRightIR2();
+  int rotated = 0;
+  while (abs(diff) >= 0.4 && getRightIR1_Block() == getRightIR2_Block() && rotated < 15) {
+    rotated++;
+    if (diff > 0) {
+      rotateLeft(abs(diff * 5));
+      diff = getRightIR1() - getRightIR2();
+      if (getRightIR1_Block() != getRightIR2_Block()) {
+        rotateRight(abs(diff * 4));
+        diff = getRightIR1() - getRightIR2();
+      }
+    } else {
+      rotateRight(abs(diff * 5));
+      diff = getRightIR1() - getRightIR2();
+      if (getRightIR1_Block() != getRightIR2_Block()) {
+        rotateLeft(abs(diff * 4));
+        diff = getRightIR1() - getRightIR2();
+      }
+    }
+    delay(1);
+  }
+  delay(2);
+}
+
+void alignFront() {
+  delay(2);
+  double diff_dis;
+  int moved = 0;
+  double previous_turn = 0;
+  if (getFrontIR1_Block() != 1 || getFrontIR3_Block() != 1 ) {
+    do {
+      diff_dis = getMin(getFrontIR1(), getFrontIR2(), getFrontIR3()) - DIST_WALL_CENTER_BOX;
+      if (diff_dis > 0) {
+        moveForwardCalibrate(abs(diff_dis));
+      } else {
+        moveBackwardsCalibrate(abs(diff_dis));
+      }
+      delay(2);
+      diff_dis = getMin(getFrontIR1(), getFrontIR2(), getFrontIR3()) - DIST_WALL_CENTER_BOX;
+      moved++;
+    } while (abs(diff_dis) > 0.2 && moved < 15);
+    return;
+  }
+  delay(2);
+  moved = 0;
+  double diff = getFrontIR1() - getFrontIR3();
+  while (abs(diff) >= 0.4 && moved < 15) {
+    moved++;
+    previous_turn = abs(diff * 5);
+    if (diff > 0) {
+      rotateLeft(previous_turn);
+      diff = getFrontIR1() - getFrontIR3();
+      if (getFrontIR1_Block() != getFrontIR3_Block()) {
+        rotateRight(previous_turn);
+        break;
+      }
+    } else {
+      rotateRight(previous_turn);
+      diff = getFrontIR1() - getFrontIR3();
+      if (getFrontIR1_Block() != getFrontIR3_Block()) {
+        rotateRight(previous_turn);
+        break;
+      }
+    }
+    delay(2);
+  }
+  delay(2);
+  moved = 0;
+  do {
+    diff_dis = getMin(getFrontIR1(), getFrontIR2(), getFrontIR3()) - DIST_WALL_CENTER_BOX;
+    if (diff_dis > 0) {
+      moveForwardCalibrate(abs(diff_dis));
+    } else {
+      moveBackwardsCalibrate(abs(diff_dis));
+    }
+    delay(2);
+    diff_dis = getMin(getFrontIR1(), getFrontIR2(), getFrontIR3()) - DIST_WALL_CENTER_BOX;
+    moved++;
+  } while (abs(diff_dis) > 0.2 && moved < 20);
+  moved = 0;
+  delay(2);
+  diff = getFrontIR1() - getFrontIR3();
+  while (abs(diff) >= 0.4 && moved < 15) {
+    moved++;
+    previous_turn = abs(diff * 5);
+    if (diff > 0) {
+      rotateLeft(previous_turn);
+      diff = getFrontIR1() - getFrontIR3();
+      if (getFrontIR1_Block() != getFrontIR3_Block())
+        rotateRight(previous_turn);
+    } else {
+      rotateRight(previous_turn);
+      diff = getFrontIR1() - getFrontIR3();
+      if (getFrontIR1_Block() != getFrontIR3_Block())
+        rotateRight(previous_turn);
+    }
+    delay(2);
+  }
+}
+
+
+void initializeLeftTurnEnd() {
+  //  rotateRight(8);
+  //  rotateLeft(6);
+}
+
+
+void initializeRightTurnEnd() {
+  //  rotateLeft(8);
+  //  rotateRight(6);
+}
+
+double getMin(double f1, double f2, double f3) {
+  if (f1 < f2) {
+    if (f1 < f3) {
+      return f1;
+    } else {
+      return f3;
+    }
+  } else {
+    if (f2 < f3) {
+      return f2;
+    } else {
+      return f3;
+    }
+  }
+}
+
+void leftMotorTime() {
+  tick_L++;
+}
+
+void rightMotorTime() {
+  tick_R++;
 }
 
 void initializeTick() {
-  tickCountR = 0;
-  tickCountL = 0;
-}
-
-float distToTicks(float distance) {
-  float ticks = distance / 18.85 * 562.25 * 2;
-  return ticks;
-}
-
-float rotToTicks(float angle) {
-  float ticks = angle / 360 * 56.2345 / 18.85 * 562.25 * 2 * 0.96;
-  return ticks;
+  tick_R = 0;
+  tick_L = 0;
+  speed_O = 0;
+  previous_tick_R = 0;
 }
 
 void initializeMotor_Start() {
@@ -245,16 +439,14 @@ void initializeMotor_End() {
   delay(5);
 }
 
-void stopIfFault()
-{
-  if (md.getM1Fault())
-  {
-    Serial.println("M1 fault");
-    while (1);
-  }
-  if (md.getM2Fault())
-  {
-    Serial.println("M2 fault");
-    while (1);
-  }
+int cmToTicks(int cm) {
+  int dist = (cm / 10) - 1;
+  if (dist < 10)
+    return TICKS[dist];
+  return 0;
+}
+
+int cmToTicksCalibrate(int cm) {
+  double ret = ((double)cm * TICKS[0] / 10.0) + 0.5;
+  return ret;
 }
---
+++ Arduino/arduino_checklist/sensorlib/arduinoLib.ino
@@ -0,0 +1,184 @@
+
+void setup() {
+  setupSerialConnection();
+  setupMotorEncoder();
+  setupSensorInterrupt();
+  setupPID();
+  delay(20);
+
+}
+
+boolean readSomething = false;
+void loop() {
+  delay(2);
+  if (!Serial) {
+    Serial.println("Waiting for connection");
+  }
+  //  getFrontIR1();
+  //  getFrontIR3();
+//  returnSensorReading_Raw();
+/*  if (Serial.available() > 0) {
+    char command = '0';
+    int value = -1;
+    char arr[15];
+    int c = 0;
+    char hold = Serial.read();
+    if (hold == '[') {
+      Serial.println('R');
+      Serial.flush();
+      delay(1);
+      hold = Serial.read();
+      while (hold != ']') {
+        arr[c] = hold;
+        c ++;
+        hold = Serial.read();
+        delay(2);
+      }
+    } else {
+      while (Serial.available() > 0) {
+        if ((char)Serial.peek() == '[' )
+          return;
+        Serial.println("DISCARDING");
+        Serial.read();
+        delay(1);
+        return;
+      }
+    }
+    command = arr[8];
+    char value_ca[c - 10 + 1];
+    for (int g = 10; g < c; g++) {
+      value_ca[g - 10] = arr[g];
+    }
+    value_ca[c - 10] = '\0';
+    value = atoi(value_ca);
+        Serial.println(command);
+        Serial.println(value);
+
+    // Alg|Ard|0|{1-10} (Steps) [Alg|Ard|0|3]
+    //2nd Character of the Array is the Command
+
+    // W : FORWARD
+    // A: TURN_LEFT
+    // D: TURN_RIGHT
+    // S: BACKWARD
+    // V: ALIGN_FRONT
+    // B: ALIGN_RIGHT
+    // 6: SEND_SENSORS
+    */
+while (Serial.available()>0){
+ char command=Serial.read();
+ Serial.print(command);
+  int value=3;    
+
+    switch (command) {
+      case 'W':
+        Serial.print("move forward");
+        moveForward(value * 10);
+        delay(10);
+        returnSensorReading_Raw();
+        break;
+      case 'A':
+        for (int k = 0; k < value; k++) {
+          turnLeft();
+        }
+        delay(10);
+        returnSensorReading_Raw();
+        break;
+      case 'D':
+        for (int k = 0; k < value; k++) {
+          turnRight();
+        }
+        delay(10);
+        returnSensorReading_Raw();
+        break;
+      case 'S':
+        moveBackwards(value * 10);
+        delay(10);
+        returnSensorReading_Raw();
+        break;
+      case 'V':
+        alignFront();
+        returnSensorReading_Raw();
+        break;
+      case 'B':
+        alignRight();
+        returnSensorReading_Raw();
+        break;
+      case 'Z':
+        returnSensorReading_Raw();
+        break;
+        case 'U':
+        md.setSpeeds(0,0);
+        md.setBrakes(400,400);
+        break;
+    }    
+  }
+}
+
+
+
+//--------------------------Serial Codes-------------------------------
+void setupSerialConnection() {
+  Serial.begin(9600);
+  while (!Serial);
+}
+
+void returnSensorReading_Raw() {
+  Serial.print("Ard|Alg|S|1:");
+  Serial.print(getFrontIR1());
+  Serial.print(":");
+  Serial.print(getFrontIR1_Block());
+  Serial.print(",2:");
+  Serial.print(getFrontIR2());
+  Serial.print(":");
+  Serial.print(getFrontIR2_Block());
+  Serial.print(",3:");
+  Serial.print(getFrontIR3());
+  Serial.print(":");
+  Serial.print(getFrontIR3_Block());
+  Serial.print(",4:");
+  Serial.print(getRightIR1());
+  Serial.print(":");
+  Serial.print(getRightIR1_Block());
+  Serial.print(",5:");
+  Serial.print(getRightIR2());
+  Serial.print(":");
+  Serial.print(getRightIR2_Block());
+  Serial.print(",6:");
+  Serial.print(getLeftIR1());
+  Serial.print(":");
+  Serial.println(getLeftIR1_Block());
+  //  Serial.println("Ard|And|S|");
+  Serial.flush();
+}
+
+
+void returnSensorReading() {
+  Serial.print("Ard|Alg|S|1:");
+  Serial.print((int)getFrontIR1());
+  Serial.print(":");
+  Serial.print(getFrontIR1_Block());
+  Serial.print(",2:");
+  Serial.print((int)getFrontIR2());
+  Serial.print(":");
+  Serial.print(getFrontIR2_Block());
+  Serial.print(",3:");
+  Serial.print((int)getFrontIR3());
+  Serial.print(":");
+  Serial.print(getFrontIR3_Block());
+  Serial.print(",4:");
+  Serial.print((int)getRightIR1());
+  Serial.print(":");
+  Serial.print(getRightIR1_Block());
+  Serial.print(",5:");
+  Serial.print((int)getRightIR2());
+  Serial.print(":");
+  Serial.print(getRightIR2_Block());
+  Serial.print(",6:");
+  Serial.print((int)getLeftIR1());
+  Serial.print(":");
+  Serial.print(getLeftIR1_Block());
+  Serial.print("\n");
+  //  Serial.println("Ard|And|S|");
+  Serial.flush();
+}
