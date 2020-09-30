0223b3a46467d75589ff3d7f5d17231fb99f946e
xiaoqing872 <61306326+xiaoqing872@users.noreply.github.com>
2020-09-30 18:16:22 +0800

    image rec data and code

---
+++ RPI/Image-rec/pyimagesearch/config2.py
@@ -0,0 +1,36 @@
+# import the necessary packages
+import os
+
+# define the base path to the *original* input dataset and then use
+# the base path to derive the image and annotations directories
+ORIG_BASE_PATH = "01_up"
+ORIG_IMAGES = os.path.sep.join([ORIG_BASE_PATH, "images"])
+ORIG_ANNOTS = os.path.sep.join([ORIG_BASE_PATH, "annotations"])
+
+# define the base path to the *new* dataset after running our dataset
+# builder scripts and then use the base path to derive the paths to
+# our output class label directories
+BASE_PATH = "dataset+"
+POSITVE_PATH = os.path.sep.join([BASE_PATH, "01_up"])
+NEGATIVE_PATH = os.path.sep.join([BASE_PATH, "no_01_up"])
+
+# define the number of max proposals used when running selective
+# search for (1) gathering training data and (2) performing inference
+MAX_PROPOSALS = 2000
+MAX_PROPOSALS_INFER = 200
+
+# define the maximum number of positive and negative images to be
+# generated from each image
+MAX_POSITIVE = 30
+MAX_NEGATIVE = 10
+
+# initialize the input dimensions to the network
+INPUT_DIMS = (224, 224)
+
+# define the path to the output model and label binarizer
+MODEL_PATH = "image_detector_small+.h5"
+ENCODER_PATH = "label_encoder_small+.pickle"
+
+# define the minimum probability required for a positive prediction
+# (used to filter out false-positive predictions)
+MIN_PROBA = 0.6
